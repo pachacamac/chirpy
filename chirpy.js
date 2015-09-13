a711ea80e96e869bb89228642c412103c2d520b8
pachacamac <pachacamac@inboxalias.com>
2015-09-13 13:27:38 +0200

    dtmf stuff

--- chirpy.js
+++ chirpy.js
@@ -1,8 +1,7 @@
 var Chirpy = (function(options){
-  var audioStream, audioContext, gainNode, analyser, frequencies, lastClockBit = 0;
-  var freqs = [], oscillators = [];
-  var minFreq = 4000, maxFreq = 8000, freqChannels = 18;
-  for(var i=4000; i<=8000; i+=((maxFreq-minFreq)/freqChannels)){ freqs.push(i); }
+  var audioStream, audioContext, gainNode, analyser, fft;
+  var f=1;
+  var freqs = [697*f,770*f,852*f,941*f, 1209*f,1336*f,1477*f,1633*f], oscillators = [];
 
   var _init = function(stream){
     audioContext = new AudioContext() || new webkitAudioContext() || new mozAudioContext();
@@ -10,7 +9,7 @@ var Chirpy = (function(options){
     analyser = audioContext.createAnalyser();
     audioStream.connect(analyser);
     gainNode = audioContext.createGain ? audioContext.createGain() : audioContext.createGainNode();
-    gainNode.gain.value = 0.5; //volume
+    gainNode.gain.value = 0.15; //volume
     gainNode.connect(audioContext.destination);
     //create one oscillator per frequency that we're going to use
     for(var i=0; i<freqs.length; i++){
@@ -23,66 +22,68 @@ var Chirpy = (function(options){
     console.log("init done");
   };
 
-  var setState = function(bits){
-    for(var i=0; i<bits.length; i++){
-      bits[i] == 0 ? oscillators[i].disconnect() : oscillators[i].connect(gainNode);
-    }
+  var setHalfByte = function(hb){
+    var fxi = Math.floor(hb/4)+4, fyi = hb%4;
+    for(var i=0; i<oscillators.length; i++) oscillators[i].disconnect();
+    if(hb<0||hb>15)return;
+    oscillators[fxi].connect(gainNode);
+    oscillators[fyi].connect(gainNode);
   };
 
   var setByte = function(b){
-    for(var i=0; i<oscillators.length; i++) oscillators[i].disconnect();
-    oscillators[b].connect(gainNode);
+    // for(var i=0; i<oscillators.length; i++) oscillators[i].disconnect();
+    // oscillators[b].connect(gainNode);
   };
 
   var listen = function(cb){
-    window.requestAnimationFrame(function(){listen(cb);});
-    frequencies = new Uint8Array(analyser.frequencyBinCount);
-    analyser.getByteFrequencyData(frequencies);
-    var f = frequencies.length / 22000;
-    var chanBandwidth = ((Math.floor(maxFreq*f) - Math.floor(minFreq*f)) / freqChannels);
-    //minFreq = 4000, maxFreq = 8000, freqChannels = 17;
-    var maxi1 = -100, max1 = 150; //threshold
-    var maxi2 = -100, max2 = 150; //threshold
-    for(var i=Math.floor(minFreq*f); i<Math.floor(maxFreq*f);i+=1){
-      if(frequencies[i]>max1 && Math.abs(frequencies[i] - max2) > chanBandwidth){
-        max1 = frequencies[i];
-        maxi1 = i + chanBandwidth;
-      }else if(frequencies[i]>max2 && Math.abs(frequencies[i] - max1) > chanBandwidth){
-        max2 = frequencies[i];
-        maxi2 = i + chanBandwidth;
-      }
-    }
-    if(maxi1 > -1 && maxi2 > -1){
-      var chan1 = Math.floor((maxi1 - Math.floor(minFreq*f)) / chanBandwidth),
-          chan2 = Math.floor((maxi2 - Math.floor(minFreq*f)) / chanBandwidth),
-          chan;
-      chan = chan1==16 || chan1==17 ? chan2 : chan1;
-      cb(chan);
-    }
+    // window.requestAnimationFrame(function(){listen(cb);});
+    // fft = new Uint8Array(analyser.frequencyBinCount);
+    // analyser.getByteFrequencyData(fft);
+    // var f = fft.length / 22000;
+    // var chanBandwidth = ((Math.floor(maxFreq*f) - Math.floor(minFreq*f)) / freqChannels);
+    // //minFreq = 4000, maxFreq = 8000, freqChannels = 17;
+    // var maxi1 = -100, max1 = 150; //threshold
+    // var maxi2 = -100, max2 = 150; //threshold
+    // for(var i=Math.floor(minFreq*f); i<Math.floor(maxFreq*f);i+=1){
+    //   if(fft[i]>max1 && Math.abs(fft[i] - max2) > chanBandwidth){
+    //     max1 = fft[i];
+    //     maxi1 = i + chanBandwidth;
+    //   }else if(fft[i]>max2 && Math.abs(fft[i] - max1) > chanBandwidth){
+    //     max2 = fft[i];
+    //     maxi2 = i + chanBandwidth;
+    //   }
+    // }
+    // if(maxi1 > -1 && maxi2 > -1){
+    //   var chan1 = Math.floor((maxi1 - Math.floor(minFreq*f)) / chanBandwidth),
+    //       chan2 = Math.floor((maxi2 - Math.floor(minFreq*f)) / chanBandwidth),
+    //       chan;
+    //   chan = chan1==16 || chan1==17 ? chan2 : chan1;
+    //   cb(chan);
+    // }
   };
 
   var convertData = function(str){
-    var data = [], b, h1, h2, a, baseArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-    data.push(baseArray);
-    for(var i=0; i<str.length; i++){
-      b = str.charCodeAt(i);
-      h1 = b >> 4 & 0x0f;
-      h2 = b & 0x0f;
-      a = baseArray.slice();
-      a[h1] = 1; a[16] = 1;
-      data.push(a);
-      a = baseArray.slice();
-      a[h2] = 1; a[17] = 1;
-      data.push(a);
-    }
-    data.push(baseArray);
-    return data;
+    // var data = [], b, h1, h2, a, baseArray = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+    // data.push(baseArray);
+    // for(var i=0; i<str.length; i++){
+    //   b = str.charCodeAt(i);
+    //   h1 = b >> 4 & 0x0f;
+    //   h2 = b & 0x0f;
+    //   a = baseArray.slice();
+    //   a[h1] = 1; a[16] = 1;
+    //   data.push(a);
+    //   a = baseArray.slice();
+    //   a[h2] = 1; a[17] = 1;
+    //   data.push(a);
+    // }
+    // data.push(baseArray);
+    // return data;
   };
 
   var send = function(data){
     if(data.length==0) return;
-    setState(data.shift());
-    setTimeout(function(){send(data)}, 125);
+    setHalfByte(data.shift());
+    setTimeout(function(){send(data)}, 40);
   };
 
   var init = function(cb){
@@ -94,10 +95,10 @@ var Chirpy = (function(options){
 
   return {
     init: init,
-    setState: setState,
-    setByte: setByte,
+    //setState: setState,
+    //setByte: setByte,
     listen: listen,
-    convertData: convertData,
+    //convertData: convertData,
     send: send
   };
 
